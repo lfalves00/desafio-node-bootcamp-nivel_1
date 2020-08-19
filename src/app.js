2ad0c33843fa8bb7c79d71fd66ca6c867baba637
Filipe Alves <lf@lcda.com.br>
2020-08-19 12:21:02 -0300

    atualized

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,77 @@ app.use(cors());
 
 const repositories = [];
 
+// Listar repositórios
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+// Criar novo repositório
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {
+    title,
+    url,
+    techs
+  } = request.body;
+
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(newRepo);
+
+  return response.json(newRepo);
 });
 
+// Alterar repositório existente
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repo = repositories.find(repo => repo.id === id);
+
+  if (!repo) {
+    return response.status(400).send();
+  }
+
+    repo.title = title;
+    repo.url = url;
+    repo.techs = techs;
+
+    return response.json(repo);
 });
 
+// Deletar um repositório
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
+// Dar like em um repositório
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repo = repositories.find(repo => repo.id === id);
+
+  if (!repo) {
+    return response.status(400).send()
+  }
+
+  repo.likes++;
+  return response.json(repo)
+
 });
 
 module.exports = app;
